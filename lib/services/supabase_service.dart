08f7d9fc05eafaff3329abaa0d7b0f1a800c64f9
ABurhaan <66022789+ABurhaan@users.noreply.github.com>
2025-01-03 13:57:05 +0300

    Update supabase_service.dart

--- lib/services/supabase_service.dart
+++ lib/services/supabase_service.dart
@@ -4,8 +4,8 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 
 class SupabaseService {
-  static const String SUPABASE_URL = 'https://dddvtqmrsfmhzgyxynhm.supabase.co';
-  static const String SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkZHZ0cW1yc2ZtaHpneXh5bmhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4NDA4MjUsImV4cCI6MjA1MTQxNjgyNX0.be7tIteI5aYsbdO2hi0mZ17KWCld3Bg8_QiHbeXjImA';
+  static const String SUPABASE_URL = '';
+  static const String SUPABASE_ANON_KEY = '';
   static bool _isInitialized = false;
 
   static Future<void> initialize() async {
